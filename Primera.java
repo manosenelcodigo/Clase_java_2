68f31db548c9bca891e36fcf32c86913ca6790f7
#manosenelcódigo <peligrosamente@gmail.com>
2016-04-28 23:12:26 -0300

    Primera.java

---
+++ Primera.java
@@ -0,0 +1,16 @@
+package com.manosenelcodigo.primero;
+
+//esta es la declaración de una clase
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+//más no es un objeto
+public class Primera 
+{
+    public static void main(String manosenelcodigo[] )
+    {
+        Scanner nombre=new Scanner(System.in);
+       // System.out.println("hola mundo #manosenelcódigo");
+    }
+}
